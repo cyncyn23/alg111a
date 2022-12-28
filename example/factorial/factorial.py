abc774805a4d28467c383d9d17ac80ba41ae79b6
unknown <coco.30456@gmail.com>
2022-12-28 22:24:11 +0800

    hm1

--- example/factorial/factorial.py
+++ example/factorial/factorial.py
@@ -1,15 +1,6 @@
 import math
 
 def factorial(n):
-<<<<<<< HEAD
-  r = 1
-  for i in range(1,n+1):
-    r *= i
-  return r
-  
-if __name__ == '__main__':
-  print('factorial(10)=',factorial(10))
-=======
     r = 1
     for i in range(1,n+1):
         r *= i
@@ -26,4 +17,3 @@ if __name__ == '__main__':
     print('logFactorial(10)=', logFactorial(10))
     print('exp(logFactorial(10))=', math.exp(logFactorial(10)))
     assert round(math.exp(logFactorial(10)))==factorial(10)
->>>>>>> logFactorial
